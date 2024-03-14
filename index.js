88a593af57b101b1e8465e3080c5e563731e8b6e
+ <+>
2024-03-14 18:51:59 +0300

    did pasrsing strings as numbers

---
+++ index.js
@@ -0,0 +1,32 @@
+1 + 80;
+60 -40;
+2 * 3.4;
+5.0 / 2.5;
+
+function add(a, b){
+return a + b;
+}
+function subtract(a, b){
+    return a - b;
+
+}
+function multiply(a, b){
+   return a * b;
+}
+function divide(a, b){
+   return a / b;
+
+}
+
+function increment(a){
+    return a + 1;
+}
+function decrement(a){
+    return a - 1;
+}
+function preserveDecimal(string){
+    return parseFloat(string);
+}
+function makeInt(string){
+    return parseInt(string,10);
+}
